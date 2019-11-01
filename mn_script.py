cb022591faba994d0f03e3204e332eab2f1bd642
tech <misho.janew@gmail.com>
2019-11-01 21:29:20 +0000

    ff does not work from within py script, starting xterm instead

--- mn_script.py
+++ mn_script.py
@@ -29,15 +29,34 @@ def perfTest():
     dumpNodeConnections( net.hosts )
     print "Testing network connectivity"
     net.pingAll()
-    print "Testing bandwidth between h1 and h4"
+
     h1, h2 = net.get( 'h1', 'h2' )
 #CHANGE PYTHON2
-    h1.cmd('python2 server.py')
+    h1.cmdPrint('pwd')
+
+    import threading
+#    t = threading.Thread(target=h1.cmdPrint, args=('python2 server.py',))
+#    t.start()
+    h1.cmdPrint('python2 server.py&')
     time.sleep(2)
 #CHANGE USER
-    h2.cmd("runuser -l tech -c 'firefox --headless --private http://10.0.0.1:8000/player.html'")
-    time.sleep(180)
-    #net.iperf( (h1, h4) )
+    #h2.cmd('xterm')
+    #h2.cmd('echo "a" > server-output.txt')
+    #h2.cmd("runuser -l tech -c 'firefox --private --headless http://"+h1.IP()+":8000/player.html&'")
+    #h1.cmdPrint('ls -la')
+
+    t = threading.Thread(target=h2.cmd, args=('xterm',))    
+    t.start()
+    print 'sleepin`'
+    time.sleep(80)
+    s1 = net.get('s1')
+    print 'changing BW'
+    s1.intf().config(bw=5)
+    time.sleep(60)
+    print 'changing BW'    
+    s1.intf().config(bw=10)
+    time.sleep(60)
+    
     net.stop()
 
 if __name__ == '__main__':
