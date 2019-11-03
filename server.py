9e02fd4ed8a397756fa94c25d5d6853b8e1663bd
janev94 <misho.janew@gmail.com>
2019-11-03 17:26:40 +0000

    Getting server sending socket TCP_Info

--- server.py
+++ server.py
@@ -19,12 +19,16 @@ PORT = 8000
 # 18 -> SND_CWND
 
 def getTCPInfo(s):
-    fmt = "B"*7+"I"*21
-    fmt2 = "B"*8+"I"*21
-    info = s.getsockopt(socket.IPPROTO_TCP, socket.TCP_INFO, 92)
-    x = struct.unpack(fmt, info)
-    proper = struct.unpack(fmt2, info)
-    return "app_limited: %s, RCV_SSTRESH: %s, SND_SSTRESH: %s, SND_CWND: %s" % (proper[7], proper[14 + 8], proper[17+8], proper[18+8])
+    fmt_14.04 = "B"*7+"I"*21
+    fmt_18.04 = "B"*8+"I"*21
+
+    desired = fmt_18.04
+
+    struct_size = struct.calcsize(desired)
+
+    info = s.getsockopt(socket.IPPROTO_TCP, socket.TCP_INFO, struct_size)
+    x = struct.unpack(desired, info)
+    return "app_limited: %s, RCV_SSTRESH: %s, SND_SSTRESH: %s, SND_CWND: %s" % (x[7], x[21], x[24], x[25])
 
 
 def getServerInfo():
@@ -38,7 +42,7 @@ class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         print 'PATH %s' % self.path
         if 'video' in self.path:
             cpu_load = psutil.cpu_percent()
-            info = getTCPInfo(self.request)
+            info = getTCPInfo(self.wfile._sock)
             info += ", CPU_LOAD: %f" % cpu_load
 
             t = time.time() - now
