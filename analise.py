ed8b6ea871ccabdb97120b27b63341d3b5d80e3d
janev94 <misho.janew@gmail.com>
2019-11-03 17:25:31 +0000

    Adding BW limits

--- analise.py
+++ analise.py
@@ -50,6 +50,12 @@ plt.legend()
 import sys
 import getopt
 
+available_bitrates = [86000, 156000, 281000, 437000, 827000,]  #+ [1604000]
+
+bandwidth_limits = [(0, 1*10**6), (80, 1*10**6), (80, .5*10**6), (140, .5*10**6), (140, 1*10**6), (200, 1*10**6)]
+
+bw_l = [(0, 1000000), (200, 1000000)]
+
 def main():
 
 	opts, _ = getopt.getopt(sys.argv[1:], "", ['fname=', 'save', 'noshow', 'out='])
@@ -65,20 +71,32 @@ def main():
 	times, qualities, chunks = parse_file(data_file)
 
 
-	fig, ax1 = plt.subplots()
+	fig, ax1= plt.subplots()
 
-	ax1.plot(times, chunks, 'r')
+	ax1.plot(times, chunks, 'r', label='chunk #')
 
 	ax2 = ax1.twinx()
 
-	ax2.plot(times, qualities, 'b')
+	print times, qualities
+	ax2.plot(times, qualities, 'b', marker='o', label='requested bitrate')
+	ax2.set_yticks(available_bitrates)
 
+	limit_opts = {'color': 'green', 'ls': '--', 'lw': 1, 'label': 'bandwidth limit'}
+
+	ax2.axhline(1*10**6, xmax=.4, **limit_opts)
+	ax2.axhline(.5*10**6, xmin=.4, xmax=.7, **limit_opts)
+	ax2.axhline(1*10**6, xmin=.7, xmax=1, **limit_opts)
+
+	#ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+	#plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+	fig.legend(bbox_to_anchor=(0.5, 1), loc='upper center', ncol=3)	
 	fig.tight_layout()
 
 	if show:
 		plt.show()
-	if save:	
-		plt.savefig('DASH_CC_effect.pdf')
+	if save:
+		print 'saving'	
+		plt.savefig('DASH_CC_effect.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
 	main()
