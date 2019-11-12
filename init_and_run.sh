959fab3f09fd1a654762d11ec2badc163ee0ec0a
tech <misho.janew@gmail.com>
2019-11-12 16:41:05 +0000

    Process automation

---
+++ init_and_run.sh
@@ -0,0 +1,3 @@
+vagrant up
+vagrant ssh -c "cd exp && sudo ./run_exp.sh"
+
---
+++ run_exp.sh
@@ -0,0 +1,5 @@
+touch cubic.out
+python2 mn_script.py
+./quitff.sh
+cp cubic.out /vagrant/mn_res/`date +"%F-%H-%M"`_cubic.out
+
