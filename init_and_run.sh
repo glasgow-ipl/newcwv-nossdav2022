bbc121f5904cacd9f5bb6435b351344cde57b3d5
tech <misho.janew@gmail.com>
2019-12-09 19:12:14 +0000

    Creating results folder

--- init_and_run.sh
+++ init_and_run.sh
@@ -1,3 +1,2 @@
 vagrant up
-vagrant ssh -c "mkdir logs && cd scripts && make gen_mpd && sudo python2 mn_script.py"
-
+vagrant ssh -c "mkdir results && mkdir logs && cd scripts && make gen_mpd && sudo python2 mn_script.py"
