33d4d6ee2e8fb8c7717c6d49b696d0c93235dc06
janev94 <misho.janew@gmail.com>
2020-09-07 11:01:35 +0100

    Setting up nginx server on mn clients. Changing BW between switches does not work! Needs to be host<->switch

--- scripts/mn_script.py
+++ scripts/mn_script.py
@@ -4,11 +4,14 @@ from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.cli import CLI
 #from mininet.node import OVSController
 
 import time
 from threading import Thread
 import os
+import datetime
+import sys
 
 
 class SingleSwitchTopo( Topo ):
@@ -20,8 +23,8 @@ class SingleSwitchTopo( Topo ):
             host = self.addHost( 'h%s' % (h + 1),
                             cpu=.5/n )
             # 10 Mbps, 5ms delay, 2% loss, 1000 packet queue
-            self.addLink( host, switch, bw=10, delay='5ms', loss=2,
-                            max_queue_size=1000, use_htb=True )
+            self.addLink( host, switch, bw=1000, delay='5ms',
+                             )
 
 class DumbbellTopo( Topo ):
     "Dumbbell topology with n hosts."
@@ -35,9 +38,9 @@ class DumbbellTopo( Topo ):
             hosts += [host]
 
         # 10 Mbps, 5ms delay, 2% loss, 1000 packet queue
-        self.addLink( hosts[0], s1, bw=1, delay='5ms'  )
-        self.addLink( hosts[1], s2, bw=1, delay='5ms' )
-        self.addLink( s1, s2, bw=1, delay='5ms')
+        self.addLink( hosts[0], s1, bw=10000, delay='5ms'  )
+        self.addLink( hosts[1], s2, bw=10000, delay='5ms' )
+        self.addLink( s1, s2, bw=10000, delay='5ms')
 
 
 def changeLinkBw(ep1, ep2, in_bw, out_bw=-1):
@@ -50,48 +53,102 @@ def run_on_host(host, cmd):
     host.cmdPrint(cmd)
 
 
-def perfTest():
+def test_change_bw():
+    topo = SingleSwitchTopo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+
+    h1, h2 = net.get('h1', 'h2')
+
+    s1 = net.get('s1')
+    net.iperf((h1, h2))
+    
+    changeLinkBw(h1, s1, 10)
+    net.iperf((h1, h2))
+
+def doSimulation():
     "Create network and run simple performance test"
+    # Create Topology
     topo = DumbbellTopo()
     net = Mininet( topo=topo,
                host=CPULimitedHost, link=TCLink)
     net.start()
+
+    # Test connectivity
     print("Dumping host connections")
     dumpNodeConnections( net.hosts )
     print("Testing network connectivity")
     net.pingAll()
 
-    h1, h2 = net.get( 'h1', 'h2' )
+    
+
+    server, client = net.get( 'h1', 'h2' )
 #CHANGE PYTHON2
-    h1.cmdPrint('pwd')
 
-    server_thread = Thread(target=run_on_host, args=(h1, 'python2 server.py'))
-    server_thread.start()
-
+    # Get server/client config settings
+    wd = str(server.cmd('pwd'))[:-2]
     user = os.getlogin()
-    host_ip = h1.IP()
+    server_ip = server.IP()
 
-    client_thread = Thread(target=run_on_host, args=(h2, 'su - %s -c "nohup firefox --headless --private http://%s:8000/scripts/player.html"' % (user, host_ip) ))
-    client_thread.setDaemon(True) 
-    client_thread.start()
+    # Create server config
+    time_str = datetime.datetime.now().strftime('%m-%d-%H%M')
+    server_log_name = "%s_nginx_access.log" % time_str
+    server.cmd("echo 'events { } http { server { listen " + server_ip + "; root /vagrant; access_log /vagrant/logs/"
+         + server_log_name + ";} }' > nginx-conf.conf")
 
-    '''
+    # Start HTTP server
+    server_out = server.cmd("sudo nginx -c " + wd + "/nginx-conf.conf &")
+
+    time.sleep(3)
+
+    net.iperf((client, server))
+
+    client_cmd = 'su - %s -c "nohup firefox --headless --private http://%s/scripts/player.html&"' % (user, server_ip)
+    print ("Client cmd: %s " % client_cmd)
+
+    # client_thread = Thread(target=run_on_host, args=(client, client_cmd))
+    # client_thread.setDaemon(True) 
+    # client_thread.start()
+
+    # CLI(net)
+
+    client.cmd(client_cmd)
+
+    # CLI(net)
+
+    #appClient.cmd("curl http://" + appServer.IP() + "/available-fruits.html")
+
+    #client_thread = Thread(target=run_on_host, args=(h2,
+    #         'su - %s -c "nohup firefox --headless --private http://%s:8000/scripts/player.html"' % (user, server_ip) ))
+    #client_thread.setDaemon(True) 
+    #client_thread.start()
+
     s1, s2 = net.get('s1', 's2')
     print 'Waiting for 80 seconds'
     time.sleep(80)
-    print 'changing BW'
-    changeLinkBw(s1, s2, .5)
+
+    #month-day-hour:minute:second:microsecond
+    precise_time_str = "%m-%d-%H:%M:%S:%f"
+
+    print 'changing BW %s ' % datetime.datetime.now().strftime(precise_time_str)
+    changeLinkBw(server, s1, .5)
+    net.iperf((client, server))
     time.sleep(60)
-    print 'changing BW'    
-    changeLinkBw(s1, s2, 1)
-    time.sleep(60)
-    '''
+
+    print 'changing BW %s ' % datetime.datetime.now().strftime(precise_time_str)    
+    changeLinkBw(server, s1, 10)
+    net.iperf((client, server))
+    time.sleep(80)
     
     # Wait for the last sement to be requested at the server
-    server_thread.join()
+    #server_thread.join()
 
+    print("Stopping server")
+    server.cmd("sudo nginx -s stop")
+    print("Closing...")
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    perfTest()
+    doSimulation()
+    # test_change_bw()
