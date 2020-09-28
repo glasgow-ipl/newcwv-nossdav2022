d9c35e74475758ffdf89a6330b0a012146d745f2
janev94 <misho.janew@gmail.com>
2020-09-28 14:11:09 +0000

    Restricting link adjacent to primary sender in MN makes TCP misbehave (no retransmissions)

--- scripts/mn_script.py
+++ scripts/mn_script.py
@@ -16,7 +16,7 @@ import server
 
 import subprocess
 
-bw_init = 200
+bw_init = None
 class SingleSwitchTopo( Topo ):
     "Single switch connected to n hosts."
     def build( self, n=2 ):
@@ -32,29 +32,49 @@ class SingleSwitchTopo( Topo ):
 
 class DumbbellTopo( Topo ):
     "Dumbbell topology with n hosts."
+
+    _bw = None
+    _RTT = None
+
     def build( self, n=2 ):
         s1 = self.addSwitch( 's1' )
         s2 = self.addSwitch( 's2' )
         hosts = []
         for h in range(n):
             # Each host gets 50%/n of system CPU
-            host = self.addHost( 'h%s' % (h + 1), cpu=.5/n )
+            host = self.addHost( 'h%s' % (h + 1))
             hosts += [host]
 
         # 10 Mbps, 5ms delay, 2% loss, 1000 packet queue
-        bw = 200
-        delay = 5
-        bdp = bw*delay
-        self.addLink( hosts[0], s1, bw=bw, delay='%dms' % delay, max_queue_size=bdp)
-        self.addLink( hosts[1], s2, bw=bw, delay='%dms' % delay, max_queue_size=bdp)
-        self.addLink( s1, s2, bw=bw, delay='%dms' % delay, max_queue_size=bdp)
+        bw = 50 #Mbps
+        bw_init = self._bw = bw
+        RTT = self._RTT = 70 #ms
+        bdp = calculate_bdp(bw, RTT)
+        print(bdp) 
+
+        # Leaving non-bottleneck links to run at maximum capacity with default parameters
+        self.addLink( hosts[0], s1)#, bw=bw, delay='%dms' % delay, max_queue_size=bdp)
+        self.addLink( hosts[1], s2)#, bw=bw, delay='%dms' % delay, max_queue_size=bdp)
+        self.addLink( s1, s2, bw=bw, delay='%dms' % (RTT/2), max_queue_size=bdp)
 
 
-def changeLinkBw(ep1, ep2, in_bw, delay, out_bw=-1):
+def calculate_bdp(bw, RTT):
+    '''
+        Returns BDP in MTU sized packets, default MTU=1500
+        @bw -> badwidth in Mbps
+        @RTT -> round trip time in ms
+    '''
+    bdp = bw * 1000 # kbps
+    bdp /= 8 #kBps
+    bdp *= RTT # Bytes
+    bdp /= 1500 # MTU sized packets
+    return bdp
+
+def changeLinkBw(ep1, ep2, in_bw, RTT, out_bw=-1):
     link = ep1.connectionsTo(ep2)
-    bdp = in_bw * delay
+    bdp = calculate_bdp(in_bw, RTT)
     link[0][0].config(**{'bw': in_bw, 'max_queue_size': bdp})
-    link[0][1].config(**{'bw': out_bw if out_bw != -1 else in_bw, 'max_queue_size': bdp})
+    link[0][1].config(**{'bw': out_bw if out_bw != -1 else in_bw, 'max_queue_size': bdp if out_bw == -1 else calculate_bdp(out_bw, RTT)})
 
 
 def run_on_host(host, cmd):
@@ -77,20 +97,18 @@ def test_change_bw():
     net.iperf((h1, h2))
 
     setLogLevel('info')
-    #print(h1.connectionsTo(s1)[0][0].tc())
-    # CLI(net)
 
 def doSimulation():
     "Create network and run simple performance test"
 
     # Create a list to keep logging events
     logger = []
-    logger.append('initial link speed: %sMbps' % bw_init)
-
     # Create Topology
     topo = DumbbellTopo()
     net = Mininet( topo=topo,
                host=CPULimitedHost, link=TCLink)
+    logger.append('initial link speed: %sMbps' % bw_init)
+
     net.start()
     precise_time_str = "%m-%d-%H:%M:%S:%f"
     time_stamp = datetime.datetime.now().strftime('%m-%d-%H%M')
@@ -102,14 +120,10 @@ def doSimulation():
     net.pingAll()
 
     server, client = net.get( 'h1', 'h2' )
-#CHANGE PYTHON2
-
 
     pcap_path = os.path.join('/', 'vagrant', 'logs', time_stamp)
 
     print('tcpdump -w %s' % os.path.join(pcap_path, 'server.pcap'))
-    # server.cmdPrint('ls /vagrant/logs')
-
 
     s_name = os.path.join(pcap_path, 'server.pcap')
     c_name = os.path.join(pcap_path, 'client.pcap')
@@ -143,15 +157,12 @@ def doSimulation():
     # Create server config
     server_log_name = os.path.join(time_stamp, "nginx_access.log")
 
-    
-
     # optionally add buffer=32k (or some other big value for access_log)
     config_str = "events { } http { log_format tcp_info '$time_local, $msec, \"$request\", $status, $tcpinfo_rtt, $tcpinfo_rttvar, \"$tcpinfo_snd_cwnd\", $tcpinfo_rcv_space, $body_bytes_sent, \"$http_referer\", \"$http_user_agent\"'; server { listen " + server_ip + "; root /vagrant; access_log /vagrant/logs/" + server_log_name + " tcp_info;} }"
     with open('nginx-conf.conf', 'w') as f:
         f.write(config_str)
 
     # Start watchdog service
-
     server.cmd("python watchdog.py &")
     watchdog_pid = server.cmd("echo $!")
     print("watchdog pid: %s" % watchdog_pid)
@@ -170,44 +181,32 @@ def doSimulation():
     client_cmd = 'su - %s -c "nohup firefox --headless --private http://%s/scripts/player.html&"' % (user, server_ip)
     print ("Client cmd: %s " % client_cmd)
 
-    # client_thread = Thread(target=run_on_host, args=(client, client_cmd))
-    # client_thread.setDaemon(True) 
-    # client_thread.start()
-
-    # CLI(net)
-
     client.cmd(client_cmd)
 
-    # CLI(net)
-
-    #appClient.cmd("curl http://" + appServer.IP() + "/available-fruits.html")
-
-    #client_thread = Thread(target=run_on_host, args=(h2,
-    #         'su - %s -c "nohup firefox --headless --private http://%s:8000/scripts/player.html"' % (user, server_ip) ))
-    #client_thread.setDaemon(True) 
-    #client_thread.start()
-
     s1, s2 = net.get('s1', 's2')
-    print 'Waiting for 80 seconds'
+
+    print('Waiting for 80 seconds')
     time.sleep(80)
 
 
     #month-day-hour:minute:second:microsecond
-    bw_speed = .5
-    msg = 'changing BW %s %s ' % (bw_speed, datetime.datetime.now().strftime(precise_time_str))
-    print msg
-    logger.append(msg)
-    changeLinkBw(s1, s2, bw_speed, 5)
-    net.iperf((client, server))
-    time.sleep(60)
+    # bw_speed = .5
+    #msg = 'changing BW %s %s ' % (bw_speed, datetime.datetime.now().strftime(precise_time_str))
+    #print(msg)
+    #logger.append(msg)
 
-    bw_speed = 15
-    msg = 'changing BW to %s %s ' % (bw_speed, datetime.datetime.now().strftime(precise_time_str) )
-    print msg
-    logger.append(msg)    
-    changeLinkBw(s1, s2, bw_speed, 5)
-    net.iperf((client, server))
-    time.sleep(80)
+    #changeLinkBw(s1, s2, bw_speed, topo._RTT)
+    #net.iperf((client, server))
+    #time.sleep(60)
+
+    # bw_speed = 15
+    # msg = 'changing BW to %s %s ' % (bw_speed, datetime.datetime.now().strftime(precise_time_str) )
+    # print(msg)
+
+    # logger.append(msg)    
+    # changeLinkBw(s1, s2, bw_speed, topo._RTT)
+    # net.iperf((client, server))
+    # time.sleep(80)
     
     msg = "Waiting for server to finish %s" % datetime.datetime.now().strftime(precise_time_str)
     print(msg)
@@ -239,4 +238,3 @@ def doSimulation():
 if __name__ == '__main__':
     setLogLevel( 'info' )
     doSimulation()
-    # test_change_bw()
