16226a5146268080876961c8380fe6a73d782b5e
janev94 <misho.janew@gmail.com>
2020-10-06 17:56:56 +0000

    Made helper HTTP server accept post requests + removed obsolete code

--- scripts/watchdog.py
+++ scripts/watchdog.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 
+import BaseHTTPServer
 import SimpleHTTPServer
 import SocketServer
 # For monitoriong CPU usage
@@ -8,7 +9,7 @@ import psutil
 import time
 import os
 
-import threading
+import json
 
 PORT = 8000
 
@@ -20,33 +21,45 @@ PORT = 8000
 # 17 -> SND_SSTRESH
 # 18 -> SND_CWND
 
-output = []
+# def getTCPInfo(s):
+#     fmt_14_04 = "B"*7+"I"*21
+#     fmt_18_04 = "B"*8+"I"*21
 
-def getTCPInfo(s):
-    fmt_14_04 = "B"*7+"I"*21
-    fmt_18_04 = "B"*8+"I"*21
+#     desired = fmt_18_04
 
-    desired = fmt_18_04
+#     struct_size = struct.calcsize(desired)
 
-    struct_size = struct.calcsize(desired)
+#     info = s.getsockopt(socket.IPPROTO_TCP, socket.TCP_INFO, struct_size)
+#     x = struct.unpack(desired, info)
+#     return "app_limited: %s, RCV_SSTRESH: %s, SND_SSTRESH: %s, SND_CWND: %s" % (x[7], x[21], x[24], x[25])
 
-    info = s.getsockopt(socket.IPPROTO_TCP, socket.TCP_INFO, struct_size)
-    x = struct.unpack(desired, info)
-    return "app_limited: %s, RCV_SSTRESH: %s, SND_SSTRESH: %s, SND_CWND: %s" % (x[7], x[21], x[24], x[25])
+class MyHttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_OPTIONS(self):           
+        self.send_response(200, "ok")       
+        self.send_header('Access-Control-Allow-Origin', '*')                
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With") 
 
-
-class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/end':
-            print ("will close server now")
+            print("Stopping watchdog (GET)")
             self.server.server_close()
-        
 
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        if self.path == '/end':
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            data = json.loads(self.data_string)
+            with open('dashjs_metrics.json', 'w') as f:
+                json.dump(data, f)
+
+            print("Stopping watchdog (POST)")
+            self.server.server_close()
+
 
 
 def start_server():
-    handler = MyHandler
+    handler = MyHttpHandler
     httpd = SocketServer.TCPServer(("", PORT), handler)
 
     try:
@@ -57,16 +70,7 @@ def start_server():
         # Should not really ever get to this point, but if we do log will be interesting
         with open('logs/server.err', 'w') as f:
             f.write(repr(e))
-        # Write whatever output we had so far to the outfile
-        output_str = ''.join(output)
-        f = open('logs/cubic.out', 'w')
-        f.write(output_str)
-        f.close()
 
 
 if __name__ == '__main__':
-    t = threading.Thread(target=start_server)        
-    t.start()
-
-
-    t.join()
+    start_server()
