082eb89c3262ac513ac76655847d4e0e48d65ae4
janev94 <misho.janew@gmail.com>
2020-10-14 15:51:34 +0000

    Plotting bw changes in cwnd graph

--- scripts/net_utils.py
+++ scripts/net_utils.py
@@ -1,8 +1,10 @@
 import csv
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter, AutoMinorLocator
 import numpy as np
+import os
 
 def count_oscillations(values): # returns the number of oscillations in the value list (cwnd)
     changes = 0
@@ -15,17 +17,18 @@ def count_oscillations(values): # returns the number of oscillations in the valu
             if values[idx][1] > values[idx + 1][1]:
                 direction = not direction
                 changes += 1
-                print(values[idx][1], values[idx+1][1])
+                # print(values[idx][1], values[idx+1][1])
         else:
             if values[idx][1] < values[idx + 1][1]:
                 direction = not direction
                 changes += 1
-                print(values[idx][1], values[idx+1][1])
+                # print(values[idx][1], values[idx+1][1])
         
     return changes
 
 
-def plot_cwnd(cwnd_info, bw_changes):
+def gen_plot(plot_info):
+    cwnd_info = plot_info['cwnds']
     cwnd_info = np.array(cwnd_info)#, dtype=np.dtype('float64,int'))
     # time = 
     time = cwnd_info[:,0]
@@ -44,12 +47,61 @@ def plot_cwnd(cwnd_info, bw_changes):
 
     fig.set_size_inches((35, 5))
     plt.xticks(rotation=45)
-    # plt.figure(figsize=(100,100))
+
+    colors = iter(mcolors.TABLEAU_COLORS.keys())
+    colors.__next__() # discard blue from the set
+
+    bw_changes = plot_info['bw_changes']
+    for change in bw_changes:
+        ax.axvline(x=change[0], color=colors.__next__(), linewidth=4, label='bw changed to: %sMbps' % change[1])
+
+    plt.legend()
+
     plt.savefig('fig2.png')
 
-def get_cwnd(log_path):
+
+def parse_logs(log_root):
+    """[summary]
+
+    Args:
+        log_root ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    plot_info = {}
+
+    access_log_path = os.path.join(log_root, 'nginx_access.log')
+
+    cwnds, init_time = get_cwnd(access_log_path)
+    plot_info['cwnds'] = cwnds
+    plot_info['init_time'] = init_time
+
+    event_log_path = os.path.join(log_root, 'events.log')
+    bw_changes = get_bw_changes(event_log_path, init_time) 
+    plot_info['bw_changes'] = bw_changes
+
+    return plot_info
+    
+
+def get_bw_changes(event_log_path, init_time):
+    bw_change_times = []
+
+    with open(event_log_path) as f:
+        for line in f:
+            if 'changing BW' in line:
+                string_tokens = line.split()
+                change_time = datetime.datetime.strptime(string_tokens[-1], "%y-%m-%d-%H:%M:%S:%f")
+                change_bw = float(string_tokens[-2].strip())
+                change_time_rel = (change_time - init_time).total_seconds()
+                bw_change_times.append((change_time_rel, change_bw))
+
+    return bw_change_times
+
+
+def get_cwnd(access_log_path):
     cwnd_time = []
-    with open(log_path) as f:
+    with open(access_log_path) as f:
         reader = csv.reader(f)
         # find the first video segment requested and treat it as start of time
         while True:
@@ -71,11 +123,9 @@ def get_cwnd(log_path):
             cwnd = int(cwnd[1:-1]) # Remove the "" from the begining and the end of the cwnd value
             cwnd_time.append((ms_delta, cwnd))
     
-    return cwnd_time
+    return cwnd_time, time_init
 
 if __name__ == '__main__':
-    cwnds = get_cwnd('/vagrant/logs/10-12-1307/nginx_access.log')
-    print(len(cwnds))
-    print(cwnds)
-    print(count_oscillations(cwnds))
-    plot_cwnd(cwnds)
+    plot_info = parse_logs('/vagrant/logs/10-12-1307')
+    print(count_oscillations(plot_info['cwnds']))
+    # gen_plot(plot_info)
