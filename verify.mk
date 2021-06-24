b8cf256d28d08bd9b7fed33fca8d3b4480035f45
janev94 <misho.janew@gmail.com>
2021-06-24 15:01:04 +0000

    Using Reno cc for verification, forcing FTTP files to be created first

--- verify.mk
+++ verify.mk
@@ -5,10 +5,10 @@ MPD_LOCATION=data/3s/bbb.mpd
 OUT_DIR=${ROOT}/data
 IGNORE_LINK_LOSS=0
 
-REPEAT = 5
+REPEAT = 3
 RUNS = $(shell seq 1 ${REPEAT})
-ALGS = vreno newcwv
-LINKS = DSL FTTC FTTP
+ALGS = reno vreno newcwv
+LINKS = FTTP DSL FTTC
 
 LOGS = $(foreach link, ${LINKS}, $(foreach alg, ${ALGS}, $(foreach run_instance, ${RUNS}, ${ROOT}/logs/newcwv/${link}/${run_instance}_${alg}/nginx_access.log)))
 
@@ -151,6 +151,9 @@ ${ROOT}/logs/newcwv/FTTP/%_newcwv/nginx_access.log: ${ROOT}/scripts/mn_script.py
 	@echo $@
 	cd ${ROOT}/scripts && sudo python mn_script.py --log_dir $(@D) --cong_alg newcwv --network_model /vagrant/network_models/links/FTTP.json --mpd_location $(MPD_LOCATION) --dash_alg $(DASH_ALG) --ignore_link_loss $(IGNORE_LINK_LOSS)
 
+${ROOT}/logs/newcwv/FTTP/%_reno/nginx_access.log: ${ROOT}/scripts/mn_script.py ${ROOT}/${MPD_LOCATION} | ${ROOT}/logs
+	@echo $@
+	cd ${ROOT}/scripts && sudo python mn_script.py --log_dir $(@D) --cong_alg reno --network_model /vagrant/network_models/links/FTTP.json --mpd_location $(MPD_LOCATION) --dash_alg $(DASH_ALG) --ignore_link_loss $(IGNORE_LINK_LOSS)
 
 ${ROOT}/logs:
 	mkdir ${ROOT}/logs
