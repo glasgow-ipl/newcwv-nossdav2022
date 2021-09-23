7402d740544406ec7d30c94e962876cfefac23a1
janev94 <misho.janew@gmail.com>
2021-09-23 11:16:30 +0000

    Compiling and loading cc modules if they are not already available

--- scripts/enable_newcwv.sh
+++ scripts/enable_newcwv.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/bash
+
+newcwv_loaded=`sudo sysctl net.ipv4.tcp_available_congestion_control | grep newcwv`
+echo ${newcwv_loaded}
+
+if [ -z "${newcwv_loaded}" ]; then
+    cd /home/vagrant/newcwv
+    make
+    sudo make install_newcwv
+fi
+
 sudo sysctl net.ipv4.tcp_congestion_control=newcwv
--- scripts/enable_vreno.sh
+++ scripts/enable_vreno.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/bash
+
+vreno_loaded=`sudo sysctl net.ipv4.tcp_available_congestion_control | grep reno_verbose`
+echo ${vreno_loaded}
+
+if [ -z "${vreno_loaded}" ]; then
+    cd /home/vagrant/verbose_reno
+    make
+    sudo make install_vreno
+fi
+
 sudo sysctl net.ipv4.tcp_congestion_control=reno_verbose
